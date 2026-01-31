eed5551b5b43d986b49fd597add128abd8a17b55
iharouch <131390371+iharouch@users.noreply.github.com>
2026-01-31 00:34:12 +0000

    renamed hw files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="IST 488 HW Manager",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    page_icon=":material/edit:"
-)
-
-HW1 = st.Page("HW/HW1.py", title="HW 1", icon="📝")
-HW2 = st.Page("HW/HW2.py", title="HW 2", icon="📝", default=True)
+HW1 = st.Page("HW/HW1.py", title = "HW 1", icon="📝")
+HW2 = st.Page("HW/HW2.py", title = "HW 2", icon="📝", default=True)
 
 pg = st.navigation([HW1, HW2])
+st.set_page_config( 
+    #Set page title 
+    page_title = "IST 488 HW Manager", 
+    layout = "wide", 
+    initial_sidebar_state = "expanded", 
+    page_icon=":material/edit:" ) 
 pg.run()
