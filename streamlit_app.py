d7176563f802a78f86d0d246b9cd16a1289ed702
iharouch <131390371+iharouch@users.noreply.github.com>
2026-02-08 22:49:59 +0000

    created hw3 page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 HW1 = st.Page("HW/HW1.py", title = "HW 1", icon="📝")
-HW2 = st.Page("HW/HW2.py", title = "HW 2", icon="📝", default=True)
+HW2 = st.Page("HW/HW2.py", title = "HW 2", icon="📝")
+HW3 = st.Page("HW/HW3.py", title = "HW 3", icon="📝", default=True)
 
-pg = st.navigation([HW1, HW2])
+pg = st.navigation([HW1, HW2, HW3])
 st.set_page_config( 
     #Set page title 
     page_title = "IST 488 HW Manager", 
